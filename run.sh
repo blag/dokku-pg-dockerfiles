45ee4a012bdae5f02b4c554d4ff03ec65a834126
Drew Hubl <drew.hubl+github@gmail.com>
2014-06-27 06:29:49 -0600

    Rewrite Dockerfile and run.sh to use PostgreSQL 9.3 on Ubuntu Trusty

---
+++ run.sh
@@ -0,0 +1,31 @@
+#!/bin/bash
+
+service postgresql start 2>&1 > /dev/null
+
+# Check if we can login as the postgres user without 
+# Run psql in the background
+psql postgres -tAc "SELECT 1;" &
+# Grab the PID
+PID=$!
+# Wait for the command to finish
+sleep 2
+# Try to kill psql
+kill $PID
+# If psql asked for a password, the PID would still exist and kill would kill
+# it and exit normally with status 0
+# If psql ran the command successfully (and didn't require a login) then kill
+# would not be able to kill the PID and would exit with exit status 1
+NO_PSQL_PASSWORD=$?
+
+# If psql doesn't require a password, then set one and create the database
+if [[ $NO_PSQL_PASSWORD ]]; then
+	su postgres sh -c "/usr/lib/postgresql/9.3/bin/postgres --single -D /var/lib/postgresql/9.3/main -c config_file=/etc/postgresql/9.3/main/postgresql.conf" <<< "CREATE USER root WITH PASSWORD '$1';"
+	su postgres sh -c "/usr/lib/postgresql/9.3/bin/postgres --single -D /var/lib/postgresql/9.3/main -c config_file=/etc/postgresql/9.3/main/postgresql.conf" <<< "CREATE DATABASE db ENCODING 'UTF8' TEMPLATE template0;"
+	su postgres sh -c "/usr/lib/postgresql/9.3/bin/postgres --single -D /var/lib/postgresql/9.3/main -c config_file=/etc/postgresql/9.3/main/postgresql.conf" <<< "GRANT CONNECT, TEMPORARY ON DATABASE db TO root;"
+	su postgres sh -c "/usr/lib/postgresql/9.3/bin/postgres --single -D /var/lib/postgresql/9.3/main -c config_file=/etc/postgresql/9.3/main/postgresql.conf" <<< "GRANT SELECT, INSERT, UPDATE, DELETE, REFERENCES, TRIGGER, TRUNCATE ON ALL TABLES IN SCHEMA public TO root;"
+fi
+
+service postgresql stop 2>&1 > /dev/null
+
+# Start PostgreSQL
+sudo -u postgres /usr/lib/postgresql/9.3/bin/postgres -D /var/lib/postgresql/9.3/main -c config_file=/etc/postgresql/9.3/main/postgresql.conf
